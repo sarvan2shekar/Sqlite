d76786498cbf57b0a64f3f58507c78bd49649a88
Saravan Sekar Thillai <sarvan2shekar@gmail.com>
2017-08-08 00:27:53 +0530

    Added db presenter classes in the presenter.db package

--- app/src/main/java/com/testapp/sarvan/sqlite/view/MainActivity.java
+++ app/src/main/java/com/testapp/sarvan/sqlite/view/MainActivity.java
@@ -1,7 +1,7 @@
 package com.testapp.sarvan.sqlite.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.testapp.sarvan.sqlite.R;
 
